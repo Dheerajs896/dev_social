3fa1dfb7df4ad2af5acf95df732c63f25da22ecb
dheerajs123 <gyaanhack@gmail.com>
2018-10-05 11:39:16 +0530

    Working on register module

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const Mongoose = require("mongoose");
+const Schema = Mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  avatar: {
+    type: String
+  }
+});
+
+module.exports = User = Mongoose.model("users", UserSchema);
